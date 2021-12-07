96ab44a0c35672ccd11f39e0b86ede526a022881
c110181147 <C110181147@nkust.edu.tw>
2021-12-07 15:51:05 +0800

    This is my first commit

--- spider/src/test/java/spider.java
+++ spider/src/test/java/spider.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class spider {
-}
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+public class spider {
+        public static void main(String[] args) {
+            try {
+                Document doc = Jsoup.connect("https://www.ptt.cc/bbs/index.html").get();
+                System.out.println(doc.title());
+                Elements newsHeadlines = doc.select("#main-container div.b-list-container.action-bar-margin.bbs-screen  a");
+                for (Element headline : newsHeadlines) {
+                    System.out.println(headline.attr("name"));
+                    System.out.println(headline.absUrl("href"));
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+
--- spider/src/test/java/web.java
+++ spider/src/test/java/web.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class web {
+public class web {
+    public static void main(String[] args) {
+        try {
+            String url = "https://www.google.com/";
+            java.net.URI uri = java.net.URI.create(url);
+            // 獲取當前系統桌面擴充套件
+            java.awt.Desktop dp = java.awt.Desktop.getDesktop();
+            // 判斷系統桌面是否支援要執行的功能
+            if (dp.isSupported(java.awt.Desktop.Action.BROWSE)) {
+                dp.browse(uri);
+                // 獲取系統預設瀏覽器開啟連結
+            }
+        } catch (java.lang.NullPointerException e) {
+            // 此為uri為空時丟擲異常
+            e.printStackTrace();
+        } catch (java.io.IOException e) {
+            // 此為無法獲取系統預設瀏覽器
+            e.printStackTrace();
+        }
+    }
 }
+
