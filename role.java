69d1470fdb9d98c571e5b1bc4b20ec5ae1224111
yuen0625 <93572403+yuen0625@users.noreply.github.com>
2021-12-21 13:17:49 +0800

    Update role.java

--- role.java
+++ role.java
@@ -3,5 +3,18 @@ public class role {
 
         skill Archer = new skill(15, 10, 30, "弓箭手", "普攻", "大冰劍", "幻影之舞");
         Archer.Displaydata();
+        Archer.displayskill();
+        
+        System.out.println("------------------------");
+
+        skill Berserker = new skill(20, 5, 15, "狂戰士","普攻","狂暴之力","好戰者");
+        Berserker.Displaydata();
+        Berserker.displayskill();
+
+        System.out.println("------------------------");
+
+        skill Magician = new skill(10, 30, 40, "魔法師","普攻","地熱","死亡之帽");
+        Magician.Displaydata();
+        Magician.displayskill();
     }
 }
