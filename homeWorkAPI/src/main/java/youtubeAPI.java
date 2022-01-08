899cefe398990d198c38adaf09906d9af3637b92
yuen0625 <93572403+yuen0625@users.noreply.github.com>
2022-01-08 20:53:21 +0800

    Update youtubeAPI.java

--- homeWorkAPI/src/main/java/youtubeAPI.java
+++ homeWorkAPI/src/main/java/youtubeAPI.java
@@ -9,6 +9,8 @@ public class youtubeAPI {
         String[] apple = new String[3];
         String[] banana = new String[9];   
         String[] cat = new String[9];
+        String[] dog = new String[3];
+        String[] element = new String[3];
         try {
             String youtubeSearch = "阿神";
             String json = Jsoup.connect("https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + youtubeSearch + "&key=AIzaSyDUKYPZIaz4O3t5dyqClNuppGp3V9m-fPI&type=video&maxResults=3").ignoreContentType(true).execute().body();
@@ -55,6 +57,29 @@ public class youtubeAPI {
             }
         } 
         catch (Exception e) {
+            System.out.println(e);
+        }
+       
+        try {
+            for (int i = 0; i < 3; i++){
+                String json = Jsoup.connect("https://www.googleapis.com/youtube/v3/videos?part=snippet&key=AIzaSyDUKYPZIaz4O3t5dyqClNuppGp3V9m-fPI&id=" + apple[i] + "&maxResults=3").ignoreContentType(true).execute().body();
+//              System.out.println(json);
+
+                JSONObject Object = new JSONObject(json);
+                var items = Object.getJSONArray("items");
+//              System.out.println(items);
+                var array = items.getJSONObject(0);
+//              System.out.println(array);
+                var snippet = array.getJSONObject("snippet");
+//              System.out.println(snippet);
+                var publishedAt = snippet.getString("publishedAt");
+//              System.out.println(publishedAt);
+                dog[i] = publishedAt;
+                var title = snippet.getString("title");
+                System.out.println(title);
+                element[i]=title;
+            }
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
